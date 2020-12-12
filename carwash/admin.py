ed1d40db89d866d3d7fe050a6e039b5fe2e5640c
Esdras <esdrasj71@gmail.com>
2020-12-11 21:55:15 -0600

    Enrutamientos y Servicios

--- carwash/admin.py
+++ carwash/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Servicio
 
-# Register your models here.
+#SERVICIO
+admin.site.register(Servicio)
--- carwash/models.py
+++ carwash/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+#SERVICIO 
+class Servicio(models.Model):
+
+    nombre  =   models.CharField(max_length=100)
+    precio = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
