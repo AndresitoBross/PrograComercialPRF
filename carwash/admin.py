ddd0aded11f784f14ae28cbb5ec9b2c527ae94d8
Andres Rivera <riveraandres1997@gmail.com>
2020-12-11 23:43:33 -0600

    Modelos nuevos porque se arruinaron las ramas

--- carwash/admin.py
+++ carwash/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Marca
+from .models import Marca, Cliente, Servicio
 
 admin.site.register(Marca)
+admin.site.register(Cliente)
+admin.site.register(Servicio)
--- carwash/models.py
+++ carwash/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 from django.utils import timezone
 
+class Servicio(models.Model):
+
+    nombre = models.CharField(max_length=100)
+    precio = models.CharField(max_length=100)
+
+    def _str_(self):
+        return self.nombre
+        
+class Cliente(models.Model):
+    nombres = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    dpi = models.CharField(max_length=10)
+    telefono = models.CharField(max_length=20)
+    imagen = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+
+    def _str_(self):
+        return self.nombres
+
 class Marca(models.Model):
     nombre = models.CharField(max_length=50)
     Descripcion = models.TextField()
