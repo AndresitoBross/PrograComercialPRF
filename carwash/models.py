73058011df33bce0875ea062f75be9ebdae9123d
Andres Rivera <riveraandres1997@gmail.com>
2020-12-11 22:57:33 -0600

    Subiendo el modelo de marcas y su formulario

--- carwash/models.py
+++ carwash/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Marca(models.Model):
+    nombre = models.CharField(max_length=50)
+    Descripcion = models.TextField()
+
+    def Marcapost(self):
+        self.save()
+
+    def eliminar(self):
+        self.delete()
+
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name_plural="Marcas"
+
---
+++ carwash/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.marca_lista, name='marca_lista'),
+    path('marca/nueva', views.marca_nueva, name='marca_nueva'),
+    path('marca/<int:pk>/editar/', views.marca_editar, name='marca_editar'),
+    path('marca/<int:pk>/eliminar/', views.marca_eliminar, name='marca_eliminar'),
+]
