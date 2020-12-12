32a6dc0e3beb690a961ad3da6cc66a826d4b2d20
Julio Cayax <julio.cayax@gmail.com>
2020-12-12 17:09:58 -0600

    Plantillas

--- carwash/models.py
+++ carwash/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Servicio(models.Model):
 
@@ -13,11 +14,20 @@ class Cliente(models.Model):
     nombres = models.CharField(max_length=50)
     apellidos = models.CharField(max_length=50)
     dpi = models.CharField(max_length=10)
-    telefono = models.CharField(max_length=20)
+    telefono = models.CharField(max_length=8)
     imagen = models.ImageField(null=True , blank= True)
 
     def _str_(self):
         return self.nombres
+    
+    def Clientepost(self):
+        self.save()
+
+    def eliminar(self):
+        self.delete()
+    
+    class Meta:
+        verbose_name_plural="Clientes"
 
 class Marca(models.Model):
     nombre = models.CharField(max_length=50)
