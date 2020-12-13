36052aa33af1d497aad09243b33f2513615489f7
Andres Rivera <riveraandres1997@gmail.com>
2020-12-13 12:21:21 -0600

    Ejecuntado testeos

--- carwash/urls.py
+++ carwash/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('servicio/<int:pk>/editar/', views.servicio_editar, name='servicio_editar'),
     path('servicio/<int:pk>/eliminar/', views.servicio_eliminar, name='servicio_eliminar'),
     #Marca
-    path('marca', views.marca_lista, name='marca_list'),
+    path('marca', views.marca_lista, name='marca_lista'),
     path('marca/nueva', views.marca_nueva, name='marca_nueva'),
     path('marca/<int:pk>/editar/', views.marca_editar, name='marca_editar'),
     path('marca/<int:pk>/eliminar/', views.marca_eliminar, name='marca_eliminar'),
