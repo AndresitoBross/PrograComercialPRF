903c9b927e8a2976de660a4feebe5fcbd1d52c7f
Andres Rivera <riveraandres1997@gmail.com>
2020-12-13 00:11:19 -0600

    Julio no me dijo que habia terminado el registro de usuario pero ya se realizaron los testeos, llega master

--- carwash/tests/test_urls.py
+++ carwash/tests/test_urls.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, SimpleTestCase
 from django.urls import reverse, resolve
-from carwash.views import marca_lista, marca_eliminar, marca_nueva, marca_editar, cliente_lista, cliente_nuevo, cliente_editar, cliente_eliminar, lista_servicio, servicio_nuevo, servicio_editar, servicio_eliminar, automovil_editar, automovil_eliminar, automovil_lista, automovil_nuevo
+from carwash.views import marca_lista, marca_eliminar, marca_nueva, marca_editar, cliente_lista, cliente_nuevo, cliente_editar, cliente_eliminar, lista_servicio, servicio_nuevo, servicio_editar, servicio_eliminar, automovil_editar, automovil_eliminar, automovil_lista, automovil_nuevo, registerPage
 
 
 class MarcaUrls(SimpleTestCase):
@@ -77,3 +77,9 @@ class AutomovilUrls(SimpleTestCase):
     def test_url_editar_resolved(self):
         url = reverse('automovil_editar', kwargs={'pk': 1})
         self.assertEquals(resolve(url).func, automovil_editar)
+
+class RegistroUrls(SimpleTestCase):
+
+    def test_url_lista_resolved(self):
+        url = reverse('registerPage')
+        self.assertEquals(resolve(url).func, registerPage)
